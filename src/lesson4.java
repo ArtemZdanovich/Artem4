96b1f36acc8d58ba418bbded13150d95df3a2345
ArtemZdanovich <gilyen2@mail.ru>
2022-10-02 23:30:21 +0300

    task2

--- src/lesson4.java
+++ src/lesson4.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class lesson4 {
 
     public static void main(String[] args) {
+
         System.out.println("Введите число дней месяца\n");
         Scanner in = new Scanner(System.in);
         int arrSize = in.nextInt();
@@ -18,14 +19,43 @@ public class lesson4 {
         double res = 0;
         for (int a = 0; a < days.length; a++) {
             sum += days[a];
-            res = sum/arrSize;
+            res = sum / arrSize;
         }
-        System.out.println("Среднемесячная температура "+res);
+        System.out.println("Среднемесячная температура " + res);
+        //=========================================================//
+        // Task2
+        int[] arr = {7, 2, 10, 5, 9};
+        bubbleSort(arr);
+        if(arr[1] - arr[0] >= 3){
+            int sum1 = 0;
+            for (int i = 1; i < arr.length; i++) {
+                sum1 = sum1 + arr[i];
+            }
+            int ElSum = sum1 / (arr.length-1);
 
-
-
+            arr[0] = ElSum;
+            System.out.println("итоговые отзывы:");
+            for (int i = 0; i < arr.length; i++)
+             {
+                System.out.println(arr[i]);
+            }
+        }
 
     }
-
-
+    public static void bubbleSort( int[] array){
+        boolean sorted = false;
+        int temp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+    }
 }
+    
